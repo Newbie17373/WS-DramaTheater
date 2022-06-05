640e8c5e928b49a667cae8e5938343b7a38059e5
Bakerman <74716816+DanilMetelkin@users.noreply.github.com>
2022-06-05 23:18:33 +0600

    Delete js directory

--- js/cashbox.js
+++
@@ -1,8 +0,0 @@
-//cashbox starts
-let ticketBtn = document.querySelector('.ticket__button')
-let place = document.querySelector('#place')
-let row = document.querySelector('#row')
-ticketBtn.addEventListener('click', () => {
-alert(`Поздравляем, вы купили билет на спектакль, ваш ряд: ${row.value}, ваше место ${place.value}, билеты отправлены на почту`)
-})
-//cashbox end
